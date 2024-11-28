f241e905707202d3a1f5cfef7a1655e65ac0cc55
bariqadyatam <bariqadyatma@gmail.com>
2024-11-28 13:34:42 +0700

    update login api

---
+++ create.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 28 Nov 2024 pada 13.21
+-- Versi server: 10.6.17-MariaDB-cll-lve
+-- Versi PHP: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `xare7693_login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'testuser', 'test@example.com', '$2b$10$tSHHyzPQpvLmYRnNtLCkp.qGOS1qJV4J0ptpyw/4WuxiQI2EmaKQm'),
+(2, 'your_username', 'your_email@example.com', '$2b$10$Cl8I2Bq88Ph93kvtLDe5/.KlQtZC3UsBGjTzMmp8Z87jV6yErErXS');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.md
@@ -0,0 +1,16 @@
+# API Registrasi dan Login
+
+API ini menyediakan endpoint untuk registrasi, login, dan pengelolaan pengguna menggunakan Node.js, Express, dan MySQL.
+
+## Fitur
+
+- Registrasi pengguna baru
+- Login pengguna dengan token JWT
+- Melihat daftar pengguna
+- Menghapus pengguna berdasarkan ID
+
+## Persyaratan
+
+- Node.js
+- MySQL
+- npm (Node Package Manager)
