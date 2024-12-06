1d76dbb3abac3753334574ee116dcbcc9c660e88
IvanPratama <greivanpratama@gmail.com>
2024-12-06 10:54:21 +0700

    Delete app directory

--- app/src/main/java/com/dicoding/capstone/api/ApiService.kt
+++
@@ -1,8 +0,0 @@
-import com.dicoding.capstone.response.ArticleResponse
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("articles")
-    suspend fun getArticles(): Response<ArticleResponse>
-}
